580367f8ef5eec5af60047233b29102efd16b297
Lorenzo Ricciardiello <lorenzo1995111@gmail.com>
2023-09-01 19:23:51 +0100

    log/registerFinish

--- config/app.php
+++ config/app.php
@@ -158,7 +158,7 @@ return [
     'providers' => ServiceProvider::defaultProviders()->merge([
         /*
          * Package Service Providers...
-         */
+         */App\Providers\FortifyServiceProvider::class,
 
         /*
          * Application Service Providers...
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -118,9 +118,29 @@ background: linear-gradient(90deg, rgba(4,4,4,0.8688725490196079) 0%, rgba(0,0,0
             <li><a class="dropdown-item" href="">{{$category}}</a></li>
            @endforeach
       </ul>
-      </div>
-  </div>
-  
 
-</form>
+
+
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        @if(Auth::user() == null)
+          <li class="nav-item">
+            <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-secondary mx-2" href="{{ route('register') }}">Registrati</a>
+          </li>
+        @else 
+          <li class="nav-item">
+            {{ Auth::user()->name }} ({{ Auth::user()->profile }})
+          </li> 
+          <li class="nav-item">
+            <form action="{{ route('logout') }}" method="POST">
+              @csrf
+              <button class="btn btn-secondary mx-2" >Logout</button>
+            </form>
+          </li>
+        @endif
+      </ul>
+      </div>
+    </div>
 </nav>
